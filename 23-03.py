fff575263c587ae9ed166316eaa698f238de50e4
EvertuN <evertonalmeida022@gmail.com>
2022-03-25 02:22:55 -0300

    Python

---
+++ 23-03.py
@@ -0,0 +1,20 @@
+"""
+Everton Almeida - UTF-8 - pt-br - 23-02-2022
+Formulário Avaliativa
+"""
+'''
+#1
+n1=float(input('Digite sua Nota 1: '))
+n2=float(input('Digite sua Nota 2: '))
+n3=float(input('Digite sua Nota 3: '))
+n4=float(input('Digite sua Nota 4: '))
+print()
+
+print(f'Média: {n1+n2+n3+n4/4:.2f}')
+'''
+#2
+t=float(input('Tempo gasto para chegar? '))
+vm=float(input('Qual a velocidade média? '))
+
+print(f'Distância percorrida: {t*vm:.2f}')
+#print(f'Quantidade de litros de conbustível gasto: {}')
